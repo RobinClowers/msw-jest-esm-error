064a8e1d99a897163a9d3c3be07832f88604439b
Robin Clowers <robin.clowers@gmail.com>
2023-10-30 10:28:52 -0700

    Add msw

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { http, setupServer, HttpResponse } from "msw/node";
 
-test('renders learn react link', () => {
+export default setupServer(
+  http.get("*/api", () => {
+    return new HttpResponse(null, { status: 200 });
+  })
+);
+
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
